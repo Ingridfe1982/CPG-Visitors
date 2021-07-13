da381d6580259d9b060f0ac86e23d7ca96f4c194
Ingrid <59604613+Ingridfe1982@users.noreply.github.com>
2021-07-14 01:39:41 +0200

    correction or errors in html

---
+++ label.php
@@ -0,0 +1,8 @@
+<?php
+        var_dump($_POST);
+
+            // echo 'nom : '.$_POST["name"].'<br>';
+            // echo 'Prénom : '.$_POST["fname"].'<br>';
+            // echo 'Email : ' .$_POST["email"].'<br>';
+   
+?>
