ab34e631517d433644f69c2a2ddfa62113a2ea75
Ingrid <59604613+Ingridfe1982@users.noreply.github.com>
2021-07-14 01:58:31 +0200

    Test php

--- label.php
+++ label.php
@@ -1,8 +1,9 @@
 <?php
-        var_dump($_POST);
-
-            // echo 'nom : '.$_POST["name"].'<br>';
-            // echo 'Prénom : '.$_POST["fname"].'<br>';
-            // echo 'Email : ' .$_POST["email"].'<br>';
+        var_dump($_POST);?>
+        <br>
+        <?php
+            echo 'nom : '.$_POST["name"].'<br>';
+            echo 'Prénom : '.$_POST["fname"].'<br>';
+            echo 'Email : ' .$_POST["email"].'<br>';
    
-?>
+        ?>
