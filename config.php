ac701b0ac51cb95a25979b56a930441d863594d4
Ingrid <59604613+Ingridfe1982@users.noreply.github.com>
2021-07-15 01:24:00 +0200

    add connection to db

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','u868520261_ingrwf09');
+define('DB_PASS','Ingrwf09');
+define('DB_NAME','u868520261_ingrwf09');
+
+$connect = @new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($connect->connect_error) {
+    echo 'Erreur de connexion à la base de données';
+    exit;
+}
+else {
+    $connect->set_charset("utf8");
+}
+
+session_start();
+
+
+//myPrint_r($connect);
+?>
