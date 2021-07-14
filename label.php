8c424ca0799ad067b89bf6020528606cfe6dfa59
Ingrid <59604613+Ingridfe1982@users.noreply.github.com>
2021-07-14 16:09:59 +0200

    create label.php

--- label.php
+++ label.php
@@ -1,9 +1,42 @@
 <?php
         var_dump($_POST);?>
         <br>
-        <?php
-            echo 'nom : '.$_POST["name"].'<br>';
-            echo 'Prénom : '.$_POST["fname"].'<br>';
-            echo 'Email : ' .$_POST["email"].'<br>';
-   
-        ?>
+ 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <title>Etiquette</title>
+</head>
+<body>
+    <div class="container">
+    <div class="card">
+
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-96x96">
+                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">       
+            <?php
+            echo $_POST["name"].'<br>';
+            echo $_POST["fname"].'<br>';
+            echo $_POST["email"].'<br>';?> </p>
+            <p>emplacement choix cours</p>
+            <p>emplacement choix personnel</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            <p>emplacement QR code (visit_ID)</p>
+          </div>
+        </div>
+      </div>
+      </div>
+</body>
+</html>
--- main.js
+++ main.js
@@ -1,63 +1,64 @@
-function start() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('take-photo');
+// FONCTION POUR RECUPERER LE FLUX VIDEO ET CAPTURER PHOTO
+// function start() {
+//     video = document.getElementById('video');
+//     canvas = document.getElementById('canvas');
+//     photo = document.getElementById('photo');
+//     startbutton = document.getElementById('take-photo');
 
-    // accéder à la vidéo de la webcam
-    navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: false
-        })
-        //stream dans l'id vidéo
-        .then(function(stream) {
-            video.srcObject = stream;
-            video.play();
-        })
-        .catch(function(err) {
-            console.log("An error occurred: " + err);
-        });
+//     // accéder à la vidéo de la webcam
+//     navigator.mediaDevices.getUserMedia({
+//             video: true,
+//             audio: false
+//         })
+//         //stream dans l'id vidéo
+//         .then(function(stream) {
+//             video.srcObject = stream;
+//             video.play();
+//         })
+//         .catch(function(err) {
+//             console.log("An error occurred: " + err);
+//         });
 
-    video.addEventListener('canplay', function(ev) {
-        if (!streaming) {
-            height = video.videoHeight / (video.videoWidth / width);
+//     video.addEventListener('canplay', function(ev) {
+//         if (!streaming) {
+//             height = video.videoHeight / (video.videoWidth / width);
 
-            if (isNaN(height)) {
-                height = width / (4 / 3);
-            }
+//             if (isNaN(height)) {
+//                 height = width / (4 / 3);
+//             }
 
-            video.setAttribute('width', width);
-            video.setAttribute('height', height);
-            canvas.setAttribute('width', width);
-            canvas.setAttribute('height', height);
-            streaming = true;
-        }
-    }, false);
+//             video.setAttribute('width', width);
+//             video.setAttribute('height', height);
+//             canvas.setAttribute('width', width);
+//             canvas.setAttribute('height', height);
+//             streaming = true;
+//         }
+//     }, false);
 
-    startbutton.addEventListener('click', function(ev) {
-        takepicture();
-        ev.preventDefault();
-    }, false);
+//     startbutton.addEventListener('click', function(ev) {
+//         takepicture();
+//         ev.preventDefault();
+//     }, false);
 
-    clearphoto();
-}
-window.addEventListener('load', start, false);
+//     clearphoto();
+// }
+// window.addEventListener('load', start, false);
 
-function clearphoto() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+// function clearphoto() {
+//     var context = canvas.getContext('2d');
+//     context.fillStyle = "#AAA";
+//     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-}
+//     var data = canvas.toDataURL('image/png');
+//     photo.setAttribute('src', data);
+// }
 
-function takepicture() {
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
+// function takepicture() {
+//     canvas.width = width;
+//     canvas.height = height;
+//     canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+//     var data = canvas.toDataURL('image/png');
+//     photo.setAttribute('src', data);
+//   }
 
 
